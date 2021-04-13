26efd898a0056ded8eb4106f391148d6a52d567d
masahiko1214 <78416024+masahiko1214@users.noreply.github.com>
2021-04-13 18:34:51 +0900

    クラスの復習

--- first.js
+++ first.js
@@ -70,20 +70,3 @@ const greet = () =>{
 console.log("こんにちは！");
 }
 
-//クラスについて
-class Animal {
-  // 引数に「name」と「age」を追加
-  constructor(name,age) {
-    // 「"レオ"」の代わりに引数nameの値を代入
-    this.name = name;
-    
-    // 「3」の代わりに引数ageの値を代入
-    this.age = age;
-  }
-}
-
-// 引数に「"モカ"」と「8」を渡す
-const animal = new Animal("モカ",8);
-
-console.log(`名前: ${animal.name}`);
-console.log(`年齢: ${animal.age}`);
---
+++ script.js
@@ -0,0 +1,5 @@
+// Dogクラスをインポート
+import Dog from "./dog";
+
+const dog = new Dog("レオ", 4, "チワワ");
+dog.info();
