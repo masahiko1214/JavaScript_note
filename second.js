d6eee371cb9559312efbcb4858e2d4f5938d157d
masahiko1214 <78416024+masahiko1214@users.noreply.github.com>
2021-04-13 20:22:58 +0900

    mapメソッド使用

--- second.js
+++ second.js
@@ -6,3 +6,21 @@ const foundNumber = numbers.find((number)=>{
 });
 
 console.log( foundNumber);
+
+
+
+
+const names = [
+  {firstName: "Kate", lastName: "Jones"},
+	{firstName: "John", lastName: "Smith"},
+	{firstName: "Denis", lastName: "Williams"},
+	{firstName: "David", lastName: "Black"}
+];
+
+// 定数namesにmapメソッドを使って新しい配列を作り、定数fullNamesに代入
+const fullNames = names.map((name)=>{
+  return name.firstName + name.lastName;
+});
+
+// 定数fullNamesを出力
+console.log(fullNames);
