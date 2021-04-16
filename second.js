32030ae5c1d841e76b7a924903e7d9a55dd8e4c8
masahiko1214 <78416024+masahiko1214@users.noreply.github.com>
2021-04-16 11:10:15 +0900

    react復習

--- second.js
+++ second.js
@@ -43,3 +43,20 @@ call(printWanko);
 call(() => {
   console.log("ひつじ仙人");
 });
+
+
+// Reactをインポート
+import React from 'react';
+
+// Appクラスを定義
+class App extends React.Component{
+  render(){
+  return (
+      <h1>Hello React</h1>
+    );
+}
+}
+
+// Appクラスをエクスポート
+
+export default App;
