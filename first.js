fdc646f0a9a6fb967eb77e2b4a7a8bdfbfb538ae
masahiko1214 <78416024+masahiko1214@users.noreply.github.com>
2021-04-17 12:44:03 +0900

    メソッドの確認

--- first.js
+++ first.js
@@ -86,3 +86,20 @@ console.log("こんにちは！");
 
 
 console.log("こんにちは！");
+
+
+// オブジェクトに格納されている関数をメソッドという
+let obj = {
+  prop1: 'value1',
+  prop2: 'value2',
+  prop3: function() {
+      console.log('value3');
+  },
+  prop4: {
+      prop5: 'value5'
+  }
+}
+obj.prop3();
+
+obj.prop6 = 'value6';
+console.log(obj);
