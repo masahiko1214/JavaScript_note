ac0621394fc3c2bbf8152450441971542d0421ce
masahiko1214 <78416024+masahiko1214@users.noreply.github.com>
2021-04-26 11:57:47 +0900

    thisとbindの使用

---
+++ 関数とオブジェクト/bindとthis/main.js
@@ -0,0 +1,17 @@
+window.name = 'John';
+
+const person = {
+    name: 'Tom',
+    hello: function() {
+        console.log('Hello ' + this.name);
+    }
+}
+person.hello();
+
+const helloTom = person.hello.bind(person);
+
+function fn(ref) {
+    ref();
+}
+
+fn(helloTom);
