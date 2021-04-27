68488522a5caafd1d8fbffd237388eadc91af152
masahiko1214 <78416024+masahiko1214@users.noreply.github.com>
2021-04-27 12:27:43 +0900

    bindとthisの確認問題2

---
+++ 関数とオブジェクト/bindとthis確認問題/main.js
@@ -0,0 +1,62 @@
+const person = {
+  name: 'Tom',
+  bye() {
+      console.log('Bye ' + this.name);
+  },
+  hello: function (greeting) {
+      console.log(greeting + ' ' + this.name);
+      return greeting + ' ' + this.name;
+  },
+  hello1s(){ 
+      setTimeout(person.hello.bind(person,'hello'), 1000);
+  }
+  /**
+   * 問題４：
+   * 1秒後に"hello Tom"
+   * と出力されるような、メソッドを
+   * personオブジェクトに追加してみてください。
+   * 
+   * 以下のように使用するものとします。
+   * `person.hello1s()` 
+   * -> 1秒後に"hello Tom"と出力
+   * 
+   * 3通りの方法で実装してみてください。
+   * １．bind
+   * ２．アロー関数
+   * ３．thisを一旦変数に代入
+   */
+
+
+  
+}
+person.hello1s()
+
+/**
+* 問題１：
+* 1秒後に"hello Tom"
+* と出力されるように、以下のコード
+* の記載を変更しましょう。
+*/
+
+const helloTom = person.hello.bind(person,'hello');
+setTimeout(helloTom, 1000);
+
+/**
+* 問題２：
+* alertで"hello Tom"
+* と出力されるように、
+* 以下のコードを変更してください。
+*/
+
+
+
+// alert(person.hello("hello"));
+
+/**
+* 問題３：
+* person.byeメソッドを１秒後に実行したかったので
+* bindを使って束縛してみましたが、コンソールには
+* "Bye"しか表示されませんでした。
+* "Bye Tom"とするためにはどうすればよいでしょうか？
+*/
+setTimeout(person.bye.bind(person), 1000);
