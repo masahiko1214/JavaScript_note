288bc79daae056a38cfd3ca52075f02d6b0f34c8
masahiko1214 <78416024+masahiko1214@users.noreply.github.com>
2021-04-28 18:45:39 +0900

    プロトタイプの継承追記2

---
+++ 関数とオブジェクト2/プロトタイプの継承/main.js
@@ -0,0 +1,20 @@
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+Person.prototype.hello = function() {
+  console.log('hello ' + this.name);
+}
+
+function Japanese(name, age){
+  // 引数にthisを渡さないとThis.name = nameのthisがわからなくなる
+  Person.call(this, name, age)
+}
+
+Japanese.prototype = Object.create(Person.prototype);
+
+const taro = new Japanese('Taro', 23);
+console.log(taro);
+
+taro.hello();
