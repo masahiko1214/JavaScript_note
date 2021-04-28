577131eaac09706530182aa7758b563c2374d3c6
masahiko1214 <78416024+masahiko1214@users.noreply.github.com>
2021-04-28 12:04:05 +0900

    プロトタイプチェーン追記

---
+++ 関数とオブジェクト2/プロトタイプチェーン/main.js
@@ -0,0 +1,19 @@
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.hello = function() {
+    console.log('Ownproperty hello ' + this.name);
+  }
+
+}
+
+// Person.prototype.hello = function() {
+//  console.log('hello ' + this.name);
+// }
+
+Object.prototype.hello = function() {
+  console.log('Object hello ' + this.name);
+}
+
+const bob = new Person('Bob', 18);
+bob.hello();
