3462ade26e8f7d276145abd00636abdddc272e90
masahiko1214 <78416024+masahiko1214@users.noreply.github.com>
2021-05-02 19:58:43 +0900

    for ofと追記

---
+++ 反復処理/for ofと反復の可能性/main.js
@@ -0,0 +1,20 @@
+// イテレーター 反復操作を行う際にしようするオブジェクト
+
+const arry = ['a', 'b', 'c'];
+
+arry[4] = 'e';
+
+Object.prototype.method = function(){}
+
+Object.defineProperty(arry,0,{
+enumerable: false
+
+})
+
+const d = Object.getOwnPropertyDescriptor(arry, 0)
+console.log(d)
+
+for(let v of arry){
+console.log(v)
+
+}
