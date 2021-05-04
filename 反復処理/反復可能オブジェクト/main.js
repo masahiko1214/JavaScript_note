d713480d03f71a52527d68bdf2189556ff55736d
masahiko1214 <78416024+masahiko1214@users.noreply.github.com>
2021-05-04 20:12:56 +0900

    反復可能オブジェクト追記

---
+++ 反復処理/反復可能オブジェクト/main.js
@@ -0,0 +1,30 @@
+const items = {
+	prop1: 'value1',
+	prop2: 'value2',
+	prop3: 'value3'
+}
+
+
+Object.prototype[Symbol.iterator] = function(){
+	const keys = Object.keys(this);
+	let i = 0;
+	let _this =this;
+return{
+
+next(){
+	let key = keys[i++];
+	console.log(_this)
+return{
+	value: [key, _this[key]],
+	done: i > keys.length
+}
+
+}
+}
+
+}
+
+/* const items = Object.entries(obj); */
+for (let item of items) {
+	console.log(item);
+}
