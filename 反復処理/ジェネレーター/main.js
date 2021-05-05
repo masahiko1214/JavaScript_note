33c176b62fa5480349809747d3e56627c489f730
masahiko1214 <78416024+masahiko1214@users.noreply.github.com>
2021-05-05 11:29:20 +0900

    ジェネレーター追記2

--- 反復処理/ジェネレーター/main.js
+++ 反復処理/ジェネレーター/main.js
@@ -1,4 +1,4 @@
-function* gen(){
+/* function* gen(){
   yield 1;
   yield 2;
   return 3;
@@ -8,4 +8,30 @@ const it = gen();
 console.log(it.next())
 console.log(it.next())
 console.log(it.next())
+console.log(it.next()) */
+
+function* genIterator(max = 10){
+  let i = 0;
+
+  while(i < max){
+    yield i++;
+  }
+  return;
+}
+
+const it = genIterator(10);
 console.log(it.next())
+
+let a = it.next();
+while(!a.done){
+   console.log(a.value); 
+  a = it.next();
+}
+
+const obj = {
+  [Symbol.iterator]: genIterator
+}
+
+for(const i of genIterator()){
+  console.log(i)
+}
