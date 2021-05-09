c444dbb5ae14b815d25aa98497aff64eb83991b2
masahiko1214 <78416024+masahiko1214@users.noreply.github.com>
2021-05-09 17:34:39 +0900

    MacrotasksとMicrotasks追記(2)

---
+++ 非同期処理/MacrotasksとMicrotasks(2)/main.js
@@ -0,0 +1,24 @@
+new Promise(function promise(resolve) {
+  console.log('promise');
+
+  setTimeout(function task1() {
+    console.log('task1');
+    resolve();
+  });
+
+}).then(function job1() {
+  console.log('job1');
+  setTimeout(function task1(){
+console.log('task2');
+  });
+queueMicrotask(function job4(){
+  console.log('job4');
+});
+
+}).then(function job1() {
+  console.log('job2');
+}).then(function job1() {
+  console.log('job3');
+})
+
+console.log('global end');
