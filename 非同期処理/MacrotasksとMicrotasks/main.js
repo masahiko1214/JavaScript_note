0614a6655bcad2af1764f2a837307e354507391e
masahiko1214 <78416024+masahiko1214@users.noreply.github.com>
2021-05-09 17:02:51 +0900

    マクロタスクとマイクロタスク追記

---
+++ 非同期処理/MacrotasksとMicrotasks/main.js
@@ -0,0 +1,14 @@
+// 下記はマクロタスク
+setTimeout(function task1() {
+  console.log('task1');
+});
+
+// 下記はマイクロタスク
+new Promise(function promise(resolve) {
+  console.log('promise');
+  resolve();
+}).then(function job1() {
+  console.log('job1');
+});
+
+console.log('global end');
