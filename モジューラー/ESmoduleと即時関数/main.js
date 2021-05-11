3bb9aff09653b8881133d9ffaf1863ccc46f94e6
masahiko1214 <78416024+masahiko1214@users.noreply.github.com>
2021-05-11 16:33:07 +0900

    モジュールと即時関数追記

---
+++ モジューラー/ESmoduleと即時関数/main.js
@@ -0,0 +1,29 @@
+const moduleA = (function () {
+
+  console.log('IIFE called');
+
+  let privateVal = 1;
+  let publicVal = 10;
+
+  function publicFn() {
+    console.log('publicFn called: ' + privateVal++);
+  }
+
+  function privateFn() {
+
+  }
+
+  return {
+    publicFn,
+    publicVal
+  }
+})();
+const moduleB = (function({publicFn,publicVal}){
+publicFn();
+publicFn();
+publicFn();
+publicFn();
+console.log(publicVal++)
+console.log(publicVal++)
+publicFn();
+})(moduleA);
---
+++ モジューラー/ESmoduleと即時関数/moduleA.js
@@ -0,0 +1,12 @@
+console.log('ESmodule called');
+
+let privateVal = 1;
+export let publicVal = 10;
+
+export function publicFn() {
+  console.log('publicFn called: ' + privateVal++);
+}
+
+function privateFn() {
+
+}
