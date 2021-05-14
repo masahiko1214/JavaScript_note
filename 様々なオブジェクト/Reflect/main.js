814454d05baa0b18d7b7a9ec5f3d2e6d26064d14
masahiko1214 <78416024+masahiko1214@users.noreply.github.com>
2021-05-14 17:58:15 +0900

    Reflect 追記

---
+++ 様々なオブジェクト/Reflect/main.js
@@ -0,0 +1,42 @@
+class C {
+  constructor(a, b) {
+    this.a = a;
+    this.b = b;
+  }
+}
+
+const obj1 = new C(1,2);
+console.log(obj1)
+const obj2 = Reflect.construct(C, [1,2]);
+console.log(obj2);
+
+console.log('a' in obj1);
+console.log(Reflect.has(obj1,'b'));
+
+
+
+/* if(Reflect.defineProperty){
+
+} else {
+
+} */
+
+ const bob = {
+   name: 'Bob',
+   _hello: function () {
+     console.log(`hello ${this.name}`);
+   }
+ }
+
+ const tom = {
+   name: 'Tom',
+   _hello: function () {
+     console.log(`hello ${this.name}`);
+   },
+   get hello() {
+     return this._hello();
+   },
+ }
+
+ tom.hello;
+ Reflect.get(tom, 'hello');
