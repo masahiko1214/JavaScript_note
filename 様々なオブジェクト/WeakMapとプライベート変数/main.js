28f2f4e56e0ac31647b7c2f65a21022a552b0975
masahiko1214 <78416024+masahiko1214@users.noreply.github.com>
2021-05-17 13:06:06 +0900

    WeakMapとプライベート変数追記

---
+++ 様々なオブジェクト/WeakMapとプライベート変数/main.js
@@ -0,0 +1,7 @@
+
+import { Person } from './person.js';
+
+const tim = new Person('Tim', 23);
+tim.hello();
+
+console.log(tim._name)
---
+++ 様々なオブジェクト/WeakMapとプライベート変数/person.js
@@ -0,0 +1,13 @@
+const wm = new WeakMap();
+export class Person {
+    constructor(name, age) {
+        // this._name = name;
+        wm.set(this,{
+            name
+        });
+    }
+
+    hello() {
+        console.log(`hello ${wm.get(this).name}`);
+    }
+}
