330fb8c641e07a90270d3fa97bc7b72bef39a00b
masahiko1214 <78416024+masahiko1214@users.noreply.github.com>
2021-05-23 14:05:20 +0900

    Array2追記

---
+++ 様々なオブジェクト/Array2/main.js
@@ -0,0 +1,19 @@
+const arry = [1, 2, 3, 4, 5];
+
+arry.forEach(function(v,i,arry){
+  console.log(i);
+});
+
+
+const newArry = arry.map(function(v,i,arry){
+  console.log(v);
+  return v*2;
+});
+
+// console.log(arry,newArry)
+
+const filterArry = arry.filter(function(v,i,arry){
+  return i >= 1;
+});
+
+console.log(filterArry)
